74b7195368e815a0ba16456db0ddc4b333bd69df
Erick Pérez <ericklperez@gmail.com>
2024-07-25 12:41:44 -0400

    Agregar archivos de proyecto.

---
+++ SupraSeguros.Integration.Messages/IntegrationBaseMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace SupraSeguros.Integration.Messages
+{
+    public class IntegrationBaseMessage
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDateTime { get; set; }
+    }
+}
---
+++ SupraSeguros.Integration.MessagingBus/IMessageBus.cs
@@ -0,0 +1,9 @@
+﻿using SupraSeguros.Integration.Messages;
+
+namespace SupraSeguros.Integration.MessagingBus
+{
+    public interface IMessageBus
+    {
+        Task PublishMessage(IntegrationBaseMessage message, string topicMessage);
+    }
+}
