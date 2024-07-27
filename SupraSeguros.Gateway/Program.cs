bf333a00de720109e4539cfd4f9ed865db1ce499
Erick Pérez <ericklperez@gmail.com>
2024-07-27 09:27:14 -0400

    Adding Gateway and changing ports

--- SupraSeguros.Gateway/Program.cs
+++ SupraSeguros.Gateway/Program.cs
@@ -1,12 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+await app.UseOcelot();
 
 app.UseHttpsRedirection();
 
